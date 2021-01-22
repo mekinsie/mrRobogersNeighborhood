f8ddd7fa6fa163d6a9c416785f7fae8d74846fa6
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-22 10:43:07 -0800

    Add more branching, pass fourth test.

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,25 @@ $(document).ready(function(){
     let result = [];
     console.log(number);
     for (let i=0; i <= number; i ++) {
-
-      if (i === 1) {
-        result.push(i);
-        result[i] = "Beep!"
-      }
-
-      else if (i != 1) {
-      result.push(i);
-      }
+    
+    if (i === 0) {
+      result.push(i + " ");
+    }
+    else if (i === 1) {
+      result.push(i + " ");
+      result[i] = "Beep! "
+    }
+    else if (i ===2) {
+      result.push(i + " ");
+      result[i] = "Boop! "
+    }
+    else if (i ===3) {
+      result.push(i+ " ");
+      result[i] = "Won't you be my nieghbor? "
+    }
+    else if (i != 1) {
+    result.push(i+ ", ");
+    }
 
     }
     $("#output").append(result);
