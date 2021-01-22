ee8705c071ca5f635855e9993e326ff0ff74bc9e
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-22 10:31:34 -0800

    Add branching, pass third test.

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,17 @@ $(document).ready(function(){
     let number = parseInt($("input#number").val());
     let result = [];
     console.log(number);
-
     for (let i=0; i <= number; i ++) {
+
+      if (i === 1) {
+        result.push(i);
+        result[i] = "Beep!"
+      }
+
+      else if (i != 1) {
       result.push(i);
+      }
+
     }
     $("#output").append(result);
     console.log(result);
