868c346f89755d23777505e268645440c9e7dd66
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-22 09:54:28 -0800

    First test passed, returns inputted number.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@
 
 
 //User Interface Logic
+$(document).ready(function(){
+  $("form.form").submit(function(event){
+    event.preventDefault();
+    let number = $("input#number").val();
+    $("#result").append(number);
+  });
+  console.log(number);
+});
+
+//let newArray = number
+//for (i=0; i <= number; i ++ )
