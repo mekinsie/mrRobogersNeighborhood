b9766a4a02bf185a2a0cd97cec752c7c0bec4d9e
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-22 14:42:21 -0800

    Change push syntax.

--- js/scripts.js
+++ js/scripts.js
@@ -9,36 +9,33 @@ $(document).ready(function(){
     
     let number =$("input#number").val();
     let result = [];
-    const priority3 = ("2","3");
-    const priority2 = ["1","2"];
-    console.log(number);
+
     for (let i=0; i <= number; i ++) {
+
+      // if (i === "3") {
+      //   result.push("Wont you be my neighbor? ");
+      // }
       
-      if (priority3.includes(number)) {
-        result.push(i + " ");
-        result[i]="Boop!";
-      }
-      
-      
-      // if (i === 0) {
-        //   result.push(i + " ");
-        // }
-        // else if (i % 20 == 1) {
-          //   result.push(i + " ");
-          //   result[i] = "Boop! "
-          // }
-          // else if (i % 10 == 1) {
-            //   result.push(i + " ");
-            //   result[i] = "Beep! "
-            // }
-            // else if (i % 10 == 2) {
-              //   result.push(i + " ");
-              //   result[i] = "Boop! "
-              // }
-              // else if (i % 10 == 3) {
-                //   result.push(i+ " ");
-                //   result[i] = "Won't you be my neighbor? "
-                // }
+      // else if (i === "2") {
+      //   result.push("Beep! ");
+      // }
+
+      // else if (i=== "1") {
+      //   result.push("Boop! ")
+      // }
+
+      if (i === 0) {
+          result.push(i + " ");
+        }
+          else if (i % 10 == 1) {
+              result.push("Beep! ");
+            }
+            else if (i % 10 == 2) {
+                result.push("Boop! ");
+              }
+              else if (i % 10 == 3) {
+                  result.push("Won't you be my neighbor? ")
+                }
                 else if (i != 1) {
                   result.push(i+ ", ");
                 }
