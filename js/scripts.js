7069e1c8eb13cbca8f52abe7ffcfa054e2b72ea2
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-22 15:40:39 -0800

    Pass sixth test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,32 @@
-// Busniess Logic
-
-
-
-//User Interface Logic
 $(document).ready(function(){
   $("form.form").submit(function(event){
     event.preventDefault();
-    
     let number =$("input#number").val();
     let result = [];
-
     for (let i=0; i <= number; i ++) {
-
-      // if (i === "3") {
-      //   result.push("Wont you be my neighbor? ");
-      // }
-      
-      // else if (i === "2") {
-      //   result.push("Beep! ");
-      // }
-
-      // else if (i=== "1") {
-      //   result.push("Boop! ")
-      // }
-
-      if (i === 0) {
-          result.push(i + " ");
-        }
-          else if (i % 10 == 1) {
-              result.push("Beep! ");
-            }
-            else if (i % 10 == 2) {
-                result.push("Boop! ");
-              }
-              else if (i % 10 == 3) {
-                  result.push("Won't you be my neighbor? ")
-                }
-                else if (i != 1) {
-                  result.push(i+ ", ");
-                }
-                
-                
-              }
-              $(".container2").show();
-              $("#output").append(result);
-              console.log(result);
-            });
-          });
+      if (i === 21) {
+        result.push("Boop! ");
+      }
+      else if (i === (31 || 33)) {
+        result.push("Won't you be my neighbor? ");
+      }
+      else if (i === 32) {
+        result.push("Won't you be my neighbor? ");
+      }
+      else if (i % 10 == 1) {
+        result.push("Beep! ");
+      }
+      else if (i % 10 == 2) {
+        result.push("Boop! ");
+      }
+      else if (i % 10 == 3) {
+        result.push("Won't you be my neighbor? ")
+      }
+      else {
+        result.push(i+ ", ");
+      }    
+    }
+    $(".container2").show();
+    $("#output").append(result);
+  });
+});
