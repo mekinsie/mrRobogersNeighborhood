71ead15e1bda10eae8c04b44b434e5da5f7022b0
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-22 13:18:56 -0800

    Test to see if other if statements prioritize 3 over 2.

--- js/scripts.js
+++ js/scripts.js
@@ -10,38 +10,33 @@ $(document).ready(function(){
     let result = [];
     console.log(number);
     for (let i=0; i <= number; i ++) {
-    
-    if (i === 0) {
-      result.push(i + " ");
-    }
-    else if (i % 10 == 1) {
-      result.push(i + " ");
-      result[i] = "Beep! "
-    }
-    else if (i % 10 ==2) {
-      result.push(i + " ");
-      result[i] = "Boop! "
-    }
-    else if (i % 10 ==3) {
-      result.push(i+ " ");
-      result[i] = "Won't you be my neighbor? "
-    }
-    // else if (i % 30 ) {
+    number.toString();
+      if (String.indexOf("1")=== true){
+        result.push(i + " ");
+        result[i] = "Boop!"
+      }
+    // if (i === 0) {
+    //   result.push(i + " ");
+    // }
+    // else if (i % 20 == 1) {
+    //   result.push(i + " ");
+    //   result[i] = "Boop! "
+    // }
+    // else if (i % 10 == 3) {
     //   result.push(i+ " ");
     //   result[i] = "Won't you be my neighbor? "
     // }
-    // else if (i % 20 ) {
-    //   result.push(i+ " ");
+    // else if (i % 10 == 2) {
+    //   result.push(i + " ");
     //   result[i] = "Boop! "
     // }
-    // else if (i % 10 ) {
-    //   result.push(i+ " ");
+    // else if (i % 10 == 1) {
+    //   result.push(i + " ");
     //   result[i] = "Beep! "
     // }
-    else if (i != 1) {
-    result.push(i+ ", ");
-    }
-
+    // else if (i != 1) {
+    // result.push(i+ ", ");
+    
     }
     $("#output").append(result);
     console.log(result);
