4c7af3939494031d49d714c090ab8c43a85c7eee
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-22 17:56:00 -0800

    Previous logic does not work. Reworking branching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
+//let functionString = function() {
+  
+
+
 $(document).ready(function(){
   $("form.form").submit(function(event){
     event.preventDefault();
     let number =$("input#number").val();
+    let array = [];
     let result = [];
     for (let i=0; i <= number; i ++) {
-      if (i % 100 == 31) {
+      array.push(i.toString());
+      if (array[i].includes("3")) {
         result.push("Won't you be my neighbor? ");
       }
-      else if (i % 100 == 32) {
-        result.push("Won't you be my neighbor? ");
-      }
-      else if (i % 100 == 21) {
-        result.push("Boop! ");
-      }
-      else if (i % 10 == 1) {
+      if (array[i].includes("2")) {
         result.push("Beep! ");
       }
-      else if (i % 10 == 2) {
+      if (array[i].includes("1")) {
         result.push("Boop! ");
       }
-      else if (i % 10 == 3) {
-        result.push("Won't you be my neighbor? ")
-      }
+      // if (i % 100 == 31) {
+      //   result.push("Won't you be my neighbor? ");
+      // }
+      // else if (i % 100 == 32) {
+      //   result.push("Won't you be my neighbor? ");
+      // }
+      // else if (i % 100 == 21) {
+      //   result.push("Boop! ");
+      // }
+      // else if (i % 10 == 1) {
+      //   result.push("Beep! ");
+      // }
+      // else if (i % 10 == 2) {
+      //   result.push("Boop! ");
+      // }
+      // else if (i % 10 == 3) {
+      //   result.push("Won't you be my neighbor? ")
+      // }
       else {
-        result.push(i+ ", ");
+        result.push(array[i]+ ", ");
       }    
     }
     $(".container2").show();
