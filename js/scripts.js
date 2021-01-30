deca8c0bd7720c48f3b20e6a6571c499caaeef9a
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-30 11:13:30 -0800

    Separate UI logic from Business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
+// Business Logic
+function numberMessage(result) {
+  let number =$("input#number").val();
+  let array = [];
+  for (let i=0; i <= number; i ++) {
+    array.push(i.toString());
+    if (array[i].includes("3")) {
+      result.push("Won't you be my neighbor? ");
+    }
+    else if (array[i].includes("2")) {
+      result.push("Boop! ");
+    }
+    else if (array[i].includes("1")) {
+      result.push("Beep! ");
+    }
+    else {
+      result.push(array[i]+ ", ");
+    }    
+  } 
+}
+
+//UI Logic
 $(document).ready(function(){
   $("form.form").submit(function(event){
     event.preventDefault();
-    let number =$("input#number").val();
-    let array = [];
     let result = [];
-    for (let i=0; i <= number; i ++) {
-      array.push(i.toString());
-      if (array[i].includes("3")) {
-        result.push("Won't you be my neighbor? ");
-      }
-      else if (array[i].includes("2")) {
-        result.push("Boop! ");
-      }
-      else if (array[i].includes("1")) {
-        result.push("Beep! ");
-      }
-      else {
-        result.push(array[i]+ ", ");
-      }    
-    }
+    numberMessage(result);
     $(".container2").show();
+    $("#output").empty();
     $("#output").append(result);
   });
 });
